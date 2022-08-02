dc1304f262b308381166d8bdc04360a625646586
unknown <bgarciakindl@gmail.com>
2022-08-02 00:46:28 -0600

    Added loops for all items and finalization code to have trip complete

--- main.py
+++ main.py
@@ -3,22 +3,23 @@ from importlib.abc import Traversable
 from logging.config import valid_ident
 from math import fabs
 import random
+from tkinter.messagebox import YES
 
 
 destinations = ["Colorado","Florida", "Texas", "New York", "Paris"]
 location_select= random.choice(destinations)
 
-def rand_location (location_select):
-    
+def rand_location (initial_location_select):
     for location in destinations:
-        print (location_select)
+        print (initial_location_select)
         user_input = input("Is this choice acceptable? yes/no: ")
 
         if user_input =="yes":
             print ("We will see you in there")
-            return location_select
+            return initial_location_select
         else:
             print ("We will select another location")
+            initial_location_select= random.choice(destinations)
 
 rand_location(location_select)
 destination = location_select
@@ -39,8 +40,9 @@ def rand_co_rest (co_rest_select):
             return co_rest_select
         else:
             print ("We will select another location")
+            co_rest_select = random.choice(co_resturaunts)
 
-resturaunt = co_rest_select
+
 
 
 co_activities =["Garden of the Gods","Cave of the Winds","Hike a 14er", "Sports Game","Denver Mint"]
@@ -54,8 +56,9 @@ def rand_co_act (co_act_select):
             return co_act_select
         else:
             print ("We will select another location")
+            co_act_select = random.choice(co_activities)
 
-activity = co_act_select
+
 
 
 fl_resturaunts  =["McGuires Irish Pub","Peg Leg Petes","Flounders","The Coffee Cup", "The District"]
@@ -70,8 +73,9 @@ def rand_fl_rest (fl_rest_select):
             return fl_rest_select
         else:
             print ("We will select another location")
+            fl_rest_select = random.choice(fl_resturaunts)
 
-resturaunt = fl_rest_select
+
 
 fl_activities  =["Jet Ski", "Lay out on the Beach", "Naval Aviation Museum", "Disney World", "Universal Studios"]
 fl_act_select = random.choice(fl_activities)
@@ -85,9 +89,9 @@ def rand_fl_act (fl_act_select):
             return fl_act_select
         else:
             print ("We will select another location")
+            fl_act_select = random.choice(fl_activities)
 
 
-activity = fl_act_select
 
 
 tx_resturaunts =["Hutchins","Contigo","BCN Taste & Tradition","The Village Bakery","Blue Bonnet Cafe"]
@@ -102,8 +106,9 @@ def rand_tx_rest (tx_rest_select):
             return tx_rest_select
         else:
             print ("We will select another location")
+            tx_rest_select =random.choice(tx_resturaunts)
 
-resturaunt = tx_rest_select
+
 
 tx_activities =["San Antonio River Walk","The Alamo","Space Center Houston","Guadalupe Mountains National Park","The Fort Worth Stockyards"]
 tx_act_select =random.choice(tx_activities)
@@ -117,8 +122,9 @@ def rand_tx_act (tx_act_select):
             return tx_act_select
         else:
             print ("We will select another location")
+            tx_act_select = random.choice(tx_activities)
 
-activity = tx_act_select
+
 
 
 ny_resturaunts =["Tavern on the Green","Nathans Famous","Peter Luger Steak House","Hudson Smokehouse","Playground"]
@@ -133,7 +139,8 @@ def rand_ny_rest (ny_rest_select):
             return ny_rest_select
         else:
             print ("We will select another location")
-resturaunt = ny_rest_select
+            ny_rest_select = random.choice(ny_resturaunts)
+
 
 ny_activities =["Central Park", "Metropolitan Museum of Art","Empire State Building","Ellis Island","Broadway Show"]
 ny_act_select =random.choice(ny_activities)
@@ -147,8 +154,8 @@ def rand_ny_act (ny_act_select):
             return ny_act_select
         else:
             print ("We will select another location")
+            ny_act_select = random.choice(ny_activities)
 
-activity = ny_act_select
 
 
 
@@ -164,8 +171,10 @@ def rand_p_rest (p_rest_select):
             return p_rest_select
         else:
             print ("We will select another location")
+            p_rest_select = random.choice(p_resturaunts)
+            
 
-resturaunt = p_rest_select
+
 
 p_activities =["Eiffel Tower","Musée du Louvre","Palais Garnier, Opéra National de Paris", "Arc de Triomphe", "Seine River Cruises"]
 p_act_select = random.choice(p_activities)
@@ -179,6 +188,8 @@ def rand_p_act (p_act_select):
             return p_act_select
         else:
             print ("We will select another location")
+            p_act_select = random.choice(p_resturaunts)
+            
 
 
 activity = p_act_select
@@ -195,7 +206,55 @@ elif destination == "New York":
 else:
     rand_p_rest (p_rest_select)
 
+if destination == "Colorado":
+    rand_co_act (co_act_select)
+elif destination == "Florida":
+    rand_fl_act (fl_act_select)
+elif destination =="Texas":
+    rand_tx_act (tx_act_select)
+elif destination == "New York":
+    rand_ny_act (ny_act_select)
+else:
+    rand_p_act (p_act_select)
+
+
+if destination == "Colorado":
+    activity = co_act_select
+    resturaunt = co_rest_select
+elif destination == "Florida":
+     activity = fl_act_select
+     resturaunt = fl_rest_select
+elif destination =="Texas":
+    activity = tx_act_select
+    resturaunt = tx_rest_select
+elif destination == "New York":
+    activity = ny_act_select
+    resturaunt = ny_rest_select
+else:
+    activity = p_act_select
+    resturaunt = p_rest_select
+   
 
 
 
+transportation_list = ["Rental Car", "Train", "Bus", "Bike", "Ride Share"]
+transport_select = random.choice(transportation_list)
+
+def rand_transport (transport_select):
+    
+    for transport in transportation_list:
+        print (transport_select)
+        user_transport_input = input("Is this choice acceptable? yes/no: ")
+        if user_transport_input=="yes":
+            print ("Thank you for selecting this transportation method")
+            return transport_select
+        else:
+            print ("We will choose another transport")
+            transport_select = random.choice(transportation_list)
+
+
+rand_transport(transport_select)
+transport = transport_select
+
+final_input = input (f"It appears that you have selected to go to {destination} and planned {activity}, and to eat at {resturaunt}, and travel by {transport} is this correct? yes/no: ")
 
